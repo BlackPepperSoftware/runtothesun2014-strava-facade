60de9c569ba2dedf3ac3f3e933c46ad5f7217332
paulspencerwilliams <paul@paulswilliams.me.uk>
2014-04-13 14:48:26 +0000

    tidied up a little

--- test/ridetothesun2014_strava_facade/test/db.clj
+++ test/ridetothesun2014_strava_facade/test/db.clj
@@ -4,9 +4,14 @@
 
 (facts "It persists data from strava and allows retreival"
        (fact "It returns json representation of metrics"
-             (update-db {:metres-climbed 123 :metres-ridden 456 :crank-rotations 789 :calories-burnt 123456} "fake-gpx")
-             (db-metrics) => 
-             {"calories-burnt" 123456 "crank-rotations" 789 "metres-climbed" 123"metres-ridden" 456})
+             (update-db {:metres-climbed 123 
+                         :metres-ridden 456 
+                         :crank-rotations 789 
+                         :calories-burnt 123456} "fake-gpx")
+             (db-metrics) => {"calories-burnt" 123456 
+                              "crank-rotations" 789 
+                              "metres-climbed" 123
+                              "metres-ridden" 456})
        (fact "It returns unchanged gpx data"
              (update-db {} "fake-gpx-data")
              (db-gpx) => "fake-gpx-data"))
--- test/ridetothesun2014_strava_facade/test/ws.clj
+++ test/ridetothesun2014_strava_facade/test/ws.clj
@@ -17,31 +17,31 @@
   (web-app {:request-method :get :uri resource :params (first params)}))
 
 (facts "It converts, rounds down and returns metrics from the database"
-  (update-db example-metrics "fake-gpx")
-  (let [resp (request "/metrics" app)
-        status (:status resp)
-        headers (:headers resp)
-        json-content (json/read-str (:body resp))]
-       (fact "and returns content type"
+       (update-db example-metrics "fake-gpx")
+       (let [resp (request "/metrics" app)
+             status (:status resp)
+             headers (:headers resp)
+             json-content (json/read-str (:body resp))]
+         (fact "and returns content type"
                (get headers "Content-Type") => "application/json")
-       (fact "returns 200 ok" status => 200)
-       (fact "metres-ridden to rounded miles" 
-             (get json-content "miles-ridden") => 20)
-       (fact "metres-climbed rounded" 
-             (get json-content "metres-climbed") => 1234)
-       (fact "crank-rotations * 4 and rounded" 
-             (get json-content "crank-rotations") => 800001)
-       (fact "calories-burnt * 4 and rounded down" 
-             (get json-content "calories-burnt") => 1056001 )
-       (fact "calories_burnt * 4 / 400 and rounded"
-             (get json-content "croissants-equivalent") => 2640)))
+         (fact "returns 200 ok" status => 200)
+         (fact "metres-ridden to rounded miles" 
+               (get json-content "miles-ridden") => 20)
+         (fact "metres-climbed rounded" 
+               (get json-content "metres-climbed") => 1234)
+         (fact "crank-rotations * 4 and rounded" 
+               (get json-content "crank-rotations") => 800001)
+         (fact "calories-burnt * 4 and rounded down" 
+               (get json-content "calories-burnt") => 1056001 )
+         (fact "calories_burnt * 4 / 400 and rounded"
+               (get json-content "croissants-equivalent") => 2640)))
 
 ;;       (fact "The facade returns gpx"
- ;;            (update-db example-metrics "fake-gpx")
-  ;;           (let [resp (request "/gpx" app)
-   ;;                status (:status resp)
-    ;;               headers (:headers resp)
-     ;;              gpx-content (:body resp)]
-      ;;         status => 200
-       ;;        (get headers "Content-Type") => "application/gpx, application/x-gpx+xml, application/xml-gpx"
-        ;;       gpx-content => "fake-gpx")))
+;;            (update-db example-metrics "fake-gpx")
+;;           (let [resp (request "/gpx" app)
+;;                status (:status resp)
+;;               headers (:headers resp)
+;;              gpx-content (:body resp)]
+;;         status => 200
+;;        (get headers "Content-Type") => "application/gpx, application/x-gpx+xml, application/xml-gpx"
+;;       gpx-content => "fake-gpx")))
